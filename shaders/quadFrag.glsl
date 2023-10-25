1ebc88401877089b0d5d230a76262e7c4f4ad7c7
James <10james.maxwell@gmail.com>
2023-10-25 21:43:42 +0100

    Added matching gaussian to quadShader

--- shaders/quadFrag.glsl
+++ shaders/quadFrag.glsl
@@ -4,12 +4,20 @@ precision mediump float;
 in vec3 fragPos;
 out vec4 FragColor;
 
+float lightCentreZ = 0.0;
+float lightStd = 0.2;
+
+// Simulate light being produced in a strip
+float lightBeamFactor(vec3 rayPos) {
+    float x = rayPos.z;
+    float gaussian = exp(-((x - lightCentreZ) * (x - lightCentreZ)) / (2.0 * lightStd * lightStd));
+    return gaussian;
+}
+
 void main()
 {
-    float pi = 3.14159;
-    float sig = 0.5;
-    float luminocity = 1.0 /(sig * sqrt(2.0 * pi)) * exp(-0.5 * pow(fragPos.z/sig, 2.0));
+    float luminocity = lightBeamFactor(fragPos);
 
-    FragColor = vec4(luminocity, luminocity, luminocity, luminocity);
+    FragColor = vec4(luminocity, luminocity, luminocity, 0.0);
     // FragColor = vec4(fragPos, luminocity);
 }
