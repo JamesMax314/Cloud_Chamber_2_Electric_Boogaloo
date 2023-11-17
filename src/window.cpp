99240cfb14fdb25a3a01d03951fab4bb9d382f15
James <10james.maxwell@gmail.com>
2023-11-17 14:57:55 +0000

    Looking good on website

--- src/window.cpp
+++ src/window.cpp
@@ -10,8 +10,8 @@ void window::framebuffer_size_callback(int width, int height)
 
 window::Window::Window()
 {
-    this->width = 1000;//EM_ASM_INT({return document.getElementById("canvas").width}, 100);
-    this->height = 1000;//EM_ASM_INT({return document.getElementById("canvas").height}, 100);
+    this->width = EM_ASM_INT({return document.getElementById("canvas").width}, 100);
+    this->height = EM_ASM_INT({return document.getElementById("canvas").height}, 100);
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
     }
@@ -25,7 +25,7 @@ window::Window::Window()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-	this->win = glfwCreateWindow(1000, 1000, "Clouds", nullptr, nullptr);
+	this->win = glfwCreateWindow(this->width, this->height, "Clouds", nullptr, nullptr);
     if (!this->win) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -48,30 +48,10 @@ window::Window::~Window(){
 	glfwDestroyWindow(this->win);
 }
 
-//void window::Window::renderFrame()
-//{
-//    rebufferLights = true;
-//
-//    for (int i=0; i<meshes.size(); i++) {
-//        if (meshes[i]->mShader) {
-//            // Setup mesh uniforms
-//
-//            meshes[i]->draw(win);
-//        }
-//    }
-//
-//    glfwSwapBuffers(win);
-//}
-
-//void window::Window::addMesh(drawable::Drawable* mesh)
-//{
-//    meshes.push_back(mesh);
-//}
-
 float window::Window::getAspect()
 {
-    width = 1000;
-    height = 1000;
+    width = this->width;
+    height = this->height;
     glViewport(0, 0, width, height);
     float aspect = (float)width / (float)height;
     return aspect;
